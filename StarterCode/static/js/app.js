8e52fbe5846db8efd0799bf7b5becf652daa9792
lleiva25 <140974405+lleiva25@users.noreply.github.com>
2023-12-26 18:14:33 -0800

    Progress made with file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,15 +9,28 @@ d3.json(url).then(function(data) {
   console.log(data);
 });
 
-function init() {
-    //Use D3 to select the dropdown menu
-    ds.select(("#selDataset"));
-    //Use D3 to select the dropdown menu
-    let dataset = dropdownMenu.property("value");
+//Use D3 to select the dropdown menu
+ds.select("#selDataset");
+
+ //Use D3 to get sample names
+ds.json(url).then((data)) => {
+    //Set a variable for sample names
+    let names = data.names;
+
+    //Add to drop down
+    names.array.forEach(id => {
+        // Log value of each iteration for every id loop
+        console.log(id);
+    });
+}
+
+
+//Use D3 to select the dropdown menu
+let dataset = dropdownMenu.property("value");
     
-    //Initialize x and y arrays
-    let x: [];
-    let y: [];
-    //type: "bar" 
+//Initialize x and y arrays
+let x: slicedData.map;
+let y: slicedData.map;
+//type: "bar" 
     
 }
